6d668e106dfb4d94bf99b7cd94b2d18d0c6d693d
Bruno Ribeiro Scholz <bruno.scholz@possible.com>
2022-05-21 18:09:41 -0300

    new Token and migrations

--- truffle-config.js
+++ truffle-config.js
@@ -44,11 +44,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -82,21 +82,16 @@ module.exports = {
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
 
-  // Set default mocha options here, use special reporters, etc.
-  mocha: {
-    // timeout: 100000
-  },
-
   // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.14",      // Fetch exact version from solc-bin (default: truffle's version)
+      optimizer: {
+        enabled: false,
+        runs: 200
+      },
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
       //  evmVersion: "byzantium"
       // }
     }
