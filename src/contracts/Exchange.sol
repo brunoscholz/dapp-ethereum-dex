32c8716a7e5f539875a9b592dc2975207e2e744f
Bruno Ribeiro Scholz <bruno.scholz@possible.com>
2022-05-23 08:50:59 -0300

    exchange deposits and withdraws done and tested

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -14,16 +14,28 @@ contract Exchange {
   mapping(address => mapping(address => uint256)) public tokens;
 
   // Events
-  event Deposit(address indexed token, address indexed user, uint256 amount, uint256 balance);
+  event Deposit(address token, address user, uint256 amount, uint256 balance);
+  event Withdraw(address token, address user, uint amount, uint balance);
 
   constructor(address _feeAccount, uint256 _feePercent) {
     feeAccount = _feeAccount;
     feePercent = _feePercent;
   }
 
+  fallback() external {
+    revert();
+  }
+
   function depositEther() payable public {
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
     emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+  }
+
+  function withdrawEther(uint _amount) payable public {
+    require(tokens[ETHER][msg.sender] >= _amount);
+    tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
+    payable(msg.sender).transfer(_amount);
+    emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
   }
 
   function depositToken(address _token, uint256 _amount) public {
@@ -33,6 +45,17 @@ contract Exchange {
     emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
   }
 
+  function withdrawToken(address _token, uint256 _amount) payable public {
+    require(_token != ETHER);
+    require(tokens[_token][msg.sender] >= _amount);
+    tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
+    require(Token(_token).transfer(msg.sender, _amount));
+    emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+  }
+
+  function balanceOf(address _token, address _user) public view returns (uint256) {
+    return tokens[_token][_user];
+  }
 }
 
 // Deposit & Withdraw Funds
@@ -40,11 +63,11 @@ contract Exchange {
 // Handle Trades - Charge fees
 // TODO:
 // [X] Set the fee
-// [ ] Deposit Ether
-// [ ] Withdraw Ether
-// [ ] Deposit tokens
-// [ ] Withdraw tokens
-// [ ] Check Balances
+// [X] Deposit Ether
+// [X] Withdraw Ether
+// [X] Deposit tokens
+// [X] Withdraw tokens
+// [X] Check Balances
 // [ ] Make Order
 // [ ] Cancel Order
 // [ ] Fill Order
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -16,7 +16,6 @@ const ether = (n) => {
 
 const tokens = (n) => ether(n)
 
-
 contract('Exchange', ([deployer, feeAccount, user1]) => {
   let token
   let exchange
@@ -37,6 +36,12 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
     it('tracks the fee percent', async () => {
       const result = await exchange.feePercent()
       result.toString().should.equal(feePercent.toString())
+    })
+  })
+
+  describe('fallback', () => {
+    it('reverts when ether is sent', async () => {
+      await exchange.sendTransaction({ value: 1, from: user1 }).should.be.rejectedWith(EVM_REVERT)
     })
   })
 
@@ -75,6 +80,42 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
       // it('fails when no tokens are approved', async () => {
       //   await exchange.depositToken(token.address, tokens(10), { from: user1 })
       // })
+    })
+  })
+
+  describe('withdrawing ether', () => {
+    let result
+    let amount
+
+    beforeEach(async () => {
+      amount = ether(1)
+      await exchange.depositEther({ from: user1, value: amount })
+    })
+
+    describe('success', () => {
+      beforeEach(async () => {
+        // withdraw Ether
+        result = await exchange.withdrawEther(amount, { from: user1 })
+      })
+
+      it('withdraws ehter funds', async() => {
+        const balance = await exchange.tokens(ETHER_ADDRESS, user1)
+        balance.toString().should.equal('0')
+      })
+
+      it('emits a Withdraw event', async () => {
+        const log = result.logs[0]
+        log.event.should.eq('Withdraw')
+        const event = log.args
+        event.token.should.equal(ETHER_ADDRESS)
+        event.user.should.equal(user1);
+        event.amount.toString().should.equal(amount.toString())
+        event.balance.toString().should.equal('0')
+      })
+    })
+
+    describe('failure', () => {
+
     })
   })
 
@@ -120,4 +161,55 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
     })
   })
 
+  describe('withdrawing tokens', () => {
+    let result
+    let amount
+
+    describe('success', () => {
+      beforeEach(async () => {
+        // deposit tokens first
+        amount = tokens(10)
+        await token.approve(exchange.address, amount, { from: user1 })
+        await exchange.depositToken(token.address, amount, { from: user1 })
+
+        result = await exchange.withdrawToken(token.address, amount, { from: user1 })
+      })
+
+      it('withdraws token funds', async() => {
+        const balance = await exchange.tokens(token.address, user1)
+        balance.toString().should.equal('0')
+      })
+
+      it('emits a Withdraw event', async () => {
+        const log = result.logs[0]
+        log.event.should.eq('Withdraw')
+        const event = log.args
+        event.token.should.equal(token.address, 'token address is correct')
+        event.user.should.equal(user1, 'user address is correct');
+        event.amount.toString().should.equal(amount.toString(), 'amount is correct')
+        event.balance.toString().should.equal('0', 'balance is correct')
+      })
+    })
+
+    describe('failure', () => {
+      it('rejects Ether withdraws', async () => {
+        await exchange.withdrawToken(ETHER_ADDRESS, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+      })
+
+      it('fails for insufficient balances', async () => {
+        await exchange.withdrawToken(token.address, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+      })
+    })
+  })
+
+  describe('checking balances', () => {
+    beforeEach(async () => {
+      await exchange.depositEther({ from: user1, value: ether(1) })
+    })
+
+    it('returns user balance', async () => {
+      const result = await exchange.balanceOf(ETHER_ADDRESS, user1)
+      result.toString().should.equal(ether(1).toString())
+    })
+  })
 })
