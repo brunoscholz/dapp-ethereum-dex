f044cb15ff3804bb6edea9434e4f330462e9a3ee
Bruno Ribeiro Scholz <bruno.scholz@possible.com>
2022-05-23 09:53:24 -0300

    exchange make and cancel orders

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -5,17 +5,48 @@ import "./Token.sol";
 import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
 contract Exchange {
-  using SafeMath for uint;
+  using SafeMath for uint256;
 
   // variables
   address public feeAccount; // the account that receives exchange fees
   uint256 public feePercent; // the fee percentage
   address constant ETHER = address(0);
   mapping(address => mapping(address => uint256)) public tokens;
+  mapping(uint256 => _Order) public orders;
+  mapping(uint256 => bool) public orderCancelled;
+  uint256 public orderCount;
 
   // Events
   event Deposit(address token, address user, uint256 amount, uint256 balance);
-  event Withdraw(address token, address user, uint amount, uint balance);
+  event Withdraw(address token, address user, uint256 amount, uint256 balance);
+  event Order (
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+  event Cancel (
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+
+  struct _Order {
+    uint256 id;
+    address user;
+    address tokenGet;
+    uint256 amountGet;
+    address tokenGive;
+    uint256 amountGive;
+    uint256 timestamp;
+  }
 
   constructor(address _feeAccount, uint256 _feePercent) {
     feeAccount = _feeAccount;
@@ -31,7 +62,7 @@ contract Exchange {
     emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
   }
 
-  function withdrawEther(uint _amount) payable public {
+  function withdrawEther(uint256 _amount) payable public {
     require(tokens[ETHER][msg.sender] >= _amount);
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
     payable(msg.sender).transfer(_amount);
@@ -56,6 +87,20 @@ contract Exchange {
   function balanceOf(address _token, address _user) public view returns (uint256) {
     return tokens[_token][_user];
   }
+
+  function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, orders[orderCount].timestamp);
+  }
+
+  function cancelOrder(uint256 _id) public {
+    _Order storage _order = orders[_id];
+    require(address(_order.user) == msg.sender); // the order must be mine
+    require(_order.id == _id); // the order must exist
+    orderCancelled[_id] = true;
+    emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, _order.timestamp);
+  }
 }
 
 // Deposit & Withdraw Funds
@@ -68,7 +113,7 @@ contract Exchange {
 // [X] Deposit tokens
 // [X] Withdraw tokens
 // [X] Check Balances
-// [ ] Make Order
-// [ ] Cancel Order
+// [X] Make Order
+// [X] Cancel Order
 // [ ] Fill Order
 // [ ] Charge fees
